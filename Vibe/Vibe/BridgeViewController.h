f73ec00a535fc57a1a36b167bc8c840b1f9e9496
Grayson Wise <graysonwise3@gmail.com>
2017-06-20 01:45:04 +0200

    downloaded and added spotify sdk, working on login

---
+++ Vibe/Vibe/BridgeViewController.h
@@ -0,0 +1,13 @@
+//
+//  BridgeViewController.h
+//  Vibe
+//
+//  Created by Grayson Wise on 6/20/17.
+//  Copyright © 2017 Vibe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BridgeViewController : UIViewController
+
+@end
