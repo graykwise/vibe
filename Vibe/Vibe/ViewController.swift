76053c1b6ef2a2c90c3e66b55fedbb75aeb7e1cc
Grayson Wise <graysonwise3@gmail.com>
2017-06-20 12:39:53 +0200

    Spotify integration almost works

--- Vibe/Vibe/ViewController.swift
+++ Vibe/Vibe/ViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import SafariServices
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SPTAudioStreamingPlaybackDelegate, SPTAudioStreamingDelegate {
 
+    @IBOutlet weak var loginButton: UIButton!
+   
+    var auth = SPTAuth.defaultInstance()!
+    var session:SPTSession!
+    var player: SPTAudioStreamingController?
+    var loginUrl: URL!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setup()
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.updateAfterFirstLogin), name: NSNotification.Name(rawValue: "loginSuccessful"), object: nil)
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +33,58 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func login(_ sender: UIButton) {
+        
+        if UIApplication.shared.canOpenURL(loginUrl!){
+            UIApplication.shared.open(loginUrl!, options: [:], completionHandler: nil)
+            
+            if auth.canHandle(auth.redirectURL) {
+                // To do - build in error handling
+            }
+        }
+
+    }
+    
+    func setup () {
+        // insert redirect your url and client ID below
+        let redirectURL = "vibe-spotify://returnafterlogin" // put your redirect URL here
+        let clientID = "c49c6284c544447b8646bbebd99aa15e" // put your client ID here
+        
+        SPTAuth.defaultInstance().clientID = clientID
+        SPTAuth.defaultInstance().redirectURL = URL(string: redirectURL)
+        auth.requestedScopes = [SPTAuthStreamingScope, SPTAuthPlaylistReadPrivateScope, SPTAuthPlaylistModifyPublicScope, SPTAuthPlaylistModifyPrivateScope]
+        loginUrl = auth.spotifyWebAuthenticationURL()
+    }
+    
+    func updateAfterFirstLogin () {
+        if let sessionObj:AnyObject = UserDefaults.standard.string(forKey: "SpotifySession") as AnyObject? {
+            let sessionDataObj = sessionObj as! Data
+            let firstTimeSession = NSKeyedUnarchiver.unarchiveObject(with: sessionDataObj) as! SPTSession
+            self.session = firstTimeSession
+            initializePlayer(authSession: session)
+        }
+    }
+    
+    func initializePlayer(authSession:SPTSession){
+        if self.player == nil {
+            self.player = SPTAudioStreamingController.sharedInstance()
+            self.player!.playbackDelegate = self
+            self.player!.delegate = self
+            try! player!.start(withClientId: auth.clientID)
+            self.player!.login(withAccessToken: authSession.accessToken)
+        }
+    }
+        
+    
+    func audioStreamingDidLogin(_ audioStreaming: SPTAudioStreamingController!) {
+        // after a user authenticates a session, the SPTAudioStreamingController is then initialized and this method called
+        print("logged in")
+        self.player?.playSpotifyURI("spotify:track:58s6EuEYJdlb0kO7awm3Vp", startingWith: 0, startingWithPosition: 0, callback: { (error) in
+            if (error != nil) {
+                print("playing!")
+            }
+        })
+    }
+    
 }
 
